9b2881acb5af866ce1c89bec4ca7e41723b1dae5
Brandt <brandtsheets@gmail.com>
2017-10-20 11:32:02 -0700

    Implement attr_accessor, finalize and dependent methods in sql_object

--- active_record_lite/lib/00_attr_accessor_object.rb
+++ active_record_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,19 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |name|
+
+      var = "@#{name}"
+      # puts "Name is #{name} and of class #{name.class}"
+      # puts "Var is #{var} and of class #{var.class}"
+      define_method(name) do
+        instance_variable_get(var)
+      end
+
+      name = (name.to_s + '=').to_sym
+      define_method(name) do |val|
+        instance_variable_set(var, val)
+      end
+    end
   end
 end
--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -5,17 +5,38 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        '#{table_name}'
+      LIMIT
+        0
+    SQL
+    @columns = cols.first.map(&:to_sym)
   end
 
   def self.finalize!
+    self.columns.each do |column|
+      define_method(column) do
+        self.attributes[column]
+      end
+
+      define_method("#{column}=") do |val|
+        self.attributes[column] = val
+      end
+    end
   end
 
   def self.table_name=(table_name)
+    @table_name = table_name
     # ...
   end
 
   def self.table_name
+    return @table_name if @table_name
+    self.to_s.downcase + 's'
     # ...
   end
 
@@ -36,10 +57,12 @@ class SQLObject
   end
 
   def attributes
+    @attributes ||= {}
     # ...
   end
 
   def attribute_values
+    @attributes.values
     # ...
   end
 
