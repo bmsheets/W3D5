d7e9d5fe7fa8165e25e013f837b752cae09ec3e1
Brandt <brandtsheets@gmail.com>
2017-10-20 14:09:20 -0700

    Complete implementation of sql_object methods

--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -41,23 +41,54 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    rows = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+    self.parse_all(rows)
+    # puts "rows is of class #{rows.class}"
+    # puts "row is of class #{rows.first.class}"
+    # rows.map do |row|
+    #   Cat.new(row.to_h)
+    # end
+    #self.parse_all(rows)
   end
 
   def self.parse_all(results)
-    # ...
+      results.map do |result|
+        Cat.new(result)
+      end
   end
 
   def self.find(id)
     # ...
+    result = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        id = #{id}
+    SQL
+    if result.empty?
+      result = nil
+    else
+      result = self.new(result.first)
+    end
+    result
   end
 
   def initialize(params = {})
     params.each do |key, value|
       setter = "#{key}=".to_sym
-      if self.class.columns.include?(key)
+      if self.class.columns.include?(key.to_sym)
         self.send(setter, value)
       else
+         puts "Current class is #{self.class}"
+         puts "Columns: #{self.class.columns}"
+         puts "Key: #{key}"
         raise "unknown attribute '#{key}'" unless attributes[key]
       end
     end
@@ -74,14 +105,35 @@ class SQLObject
   end
 
   def insert
-    # ...
+    values = self.attribute_values.map do |val|
+      "'#{val}'"
+    end
+    query = DBConnection.execute(<<-SQL)
+      INSERT INTO
+        #{self.class.table_name} (#{self.attributes.keys.join(", ")})
+      VALUES
+        (#{values.join(", ")})
+    SQL
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    set_string = self.attributes.map do |key, value|
+      "#{key} = '#{value}'"
+    end[1..-1].join(", ")
+    query = DBConnection.execute(<<-SQL)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_string}
+    SQL
   end
 
   def save
-    # ...
+    if self.id
+      update
+    else
+      insert
+    end
   end
 end
